1ed62fe2a72bb4e91f2b6992fff86b6b1ac5f00c
axel2230 <12145967+axel2230@users.noreply.github.com>
2018-11-11 18:50:36 -0400

    Add DS3231 temperature code
    Copied from https://github.com/NorthernWidget/DS3231/blob/master/DS3231.cpp

--- DS1307RTC.cpp
+++ DS1307RTC.cpp
@@ -214,5 +214,35 @@ uint8_t DS1307RTC::bcd2dec(uint8_t num)
 
 bool DS1307RTC::exists = false;
 
+float DS1307RTC::Temperature() {
+	// Checks the internal thermometer on the DS3231 and returns the 
+	// temperature as a floating-point value.
+
+  // Updated / modified a tiny bit from "Coding Badly" and "Tri-Again"
+  // http://forum.arduino.cc/index.php/topic,22301.0.html
+  
+  byte tMSB, tLSB;
+  float temp3231;
+  
+  // temp registers (11h-12h) get updated automatically every 64s
+  Wire.beginTransmission(CLOCK_ADDRESS);
+  Wire.write(0x11);
+  Wire.endTransmission();
+  Wire.requestFrom(CLOCK_ADDRESS, 2);
+
+  // Should I do more "if available" checks here?
+  if(Wire.available()) {
+    tMSB = Wire.read(); //2's complement int portion
+    tLSB = Wire.read(); //fraction portion
+
+    temp3231 = ((((short)tMSB << 8) | (short)tLSB) >> 6) / 4.0;
+  }
+  else {
+    temp3231 = -9999; // Some obvious error value
+  }
+   
+  return temp3231;
+}
+
 DS1307RTC RTC = DS1307RTC(); // create an instance for the user
 
